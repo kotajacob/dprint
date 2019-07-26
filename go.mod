4a1f2c3702520e101b012d54d6785dbcc34ec24f
Dakota Walsh <kota@nilsu.org>
2019-07-26 19:14:48 -0400

    detect options using getopt
    I've added Drew Devault's getopt implementation for option detection.

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module git.sr.ht/~kota/dprint
 
 go 1.12
+
+require git.sr.ht/~sircmpwn/getopt v0.0.0-20190621174457-292febf82fd0
--- main.go
+++ main.go
@@ -1,9 +1,30 @@
 package main
 
 import (
-	"fmt"
+	"git.sr.ht/~sircmpwn/getopt"
+	"os"
 )
 
 func main() {
-	fmt.Println("Hello World")
+	var dir, in, out string
+	opts, optind, err := getopt.Getopts(os.Args, "d:i:o:")
+	if err != nil {
+		panic(err)
+	}
+	for _, opt := range opts {
+		switch opt.Option {
+		case 'd':
+			println("Option -d specified: " + opt.Value)
+			dir = opt.Value
+		case 'i':
+			println("Option -i specified: " + opt.Value)
+			in = opt.Value
+		case 'o':
+			println("Option -o specified: " + opt.Value)
+			out = opt.Value
+		}
+	}
+	println(dir)
+	println(in)
+	println(out)
 }
