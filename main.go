e14f8f392743158257669ba8b56056a7e8f27f8e
Dakota Walsh <kota@nilsu.org>
2019-07-27 17:05:22 -0400

    fix unused variable error

--- main.go
+++ main.go
@@ -7,20 +7,17 @@ import (
 
 func main() {
 	var dir, in, out string
-	opts, optind, err := getopt.Getopts(os.Args, "d:i:o:")
+	opts, _, err := getopt.Getopts(os.Args, "d:i:o:")
 	if err != nil {
 		panic(err)
 	}
 	for _, opt := range opts {
 		switch opt.Option {
 		case 'd':
-			println("Option -d specified: " + opt.Value)
 			dir = opt.Value
 		case 'i':
-			println("Option -i specified: " + opt.Value)
 			in = opt.Value
 		case 'o':
-			println("Option -o specified: " + opt.Value)
 			out = opt.Value
 		}
 	}
